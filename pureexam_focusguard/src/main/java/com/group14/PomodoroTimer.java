c6964b7351b1fb133857d0520982b7aacff3af81
seaturtle25 <rubyding0525@gmail.com>
2026-06-03 02:18:24 +0800

    fix some bug

--- pureexam_focusguard/src/main/java/com/group14/PomodoroTimer.java
+++ pureexam_focusguard/src/main/java/com/group14/PomodoroTimer.java
@@ -8,6 +8,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.lang.reflect.Type;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
