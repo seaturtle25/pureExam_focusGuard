13c63d51b728d54d89b341d026f8f97598feb58f
Lily Ou <lilyou0721@gmail.com>
2026-06-07 17:12:27 +0800

    修專注模式：沒加其他軟體就沒加，沒有預設;考試模式：預設會鎖所有瀏覽器，可以再增加其他要鎖的軟體

--- pureexam_focusguard/src/main/java/com/group14/ProcessMonitorTest.java
+++ pureexam_focusguard/src/main/java/com/group14/ProcessMonitorTest.java
@@ -35,6 +35,9 @@ public class ProcessMonitorTest {
 
         if (PomodoroController.isExamMode) {
             //考試模式
+
+            dynamicBlackList.addAll(defaultBrowsers); //修:考試模式一開始就把預設瀏覽器加入黑名單
+
             if (PomodoroController.examBlockedApps != null) {
                 for (String appName : PomodoroController.examBlockedApps) {
                     dynamicBlackList.add(appName.toLowerCase().replace(".exe", ""));
@@ -50,10 +53,6 @@ public class ProcessMonitorTest {
                     }
                 }
             }
-        }
-
-        if (dynamicBlackList.isEmpty()) {
-            dynamicBlackList.addAll(Arrays.asList("chrome", "msedge", "firefox", "brave", "opera", "safari", "vivaldi", "tor", "chromium", "edge"));
         }
 
         executor = Executors.newSingleThreadScheduledExecutor();
